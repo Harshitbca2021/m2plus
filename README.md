b1c06454668ee0142121961fd821209dc60000ce
harshitbca2021 <harshit3687@gmail.com>
2023-01-25 19:58:03 +0530

    push

---
+++ README.md
@@ -0,0 +1,3 @@
+# m2_plus_go
+my name is sonal kumar singh
+l leave at Aurangabad
---
+++ add two number.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var SUM int
+	Num1 := 20
+	Num2 := 20
+	SUM = Num1 + Num2
+	fmt.Println("The Sum of two numbers is = ", SUM)
+}
